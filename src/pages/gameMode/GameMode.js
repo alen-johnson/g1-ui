6c53b4421843bc2de93c590dea7e32d7d270ba7b
alen <alen.l@captain.in>
2024-09-26 19:07:27 +0530

    added new modals

--- src/pages/gameMode/GameMode.js
+++ src/pages/gameMode/GameMode.js
@@ -1,9 +1,12 @@
-import { Modal } from "../../components/componentsIndex"
+import { useNavigate } from "react-router-dom"
+import { StartModal } from "../../components/componentsIndex"
 import "./GameMode.css"
 
 import React, { useState } from 'react'
 
 function GameMode() {
+
+    const navigate = useNavigate()
     const [isOpen, setIsOpen] = useState(false)
     
     const openModal = () => {
@@ -12,12 +15,15 @@ function GameMode() {
     const closeModal = () => {
         setIsOpen(false)
     }
+    const handleMutliClick = () => {
+        navigate("/creategame")
+    }
   return (
     <div className="game-mode">
         <div className="game-mode__display-area">
             <p onClick={openModal}>Single System Mode</p>
-            {isOpen && <Modal isOpen={isOpen} closeModal={closeModal}/>}
-            <p>Multi System Mode</p>
+            {isOpen && <StartModal isOpen={isOpen} closeModal={closeModal}/>}
+            <p onClick={handleMutliClick}>Multi System Mode</p>
         </div>
     </div>
   )
--- src/pages/pagesIndex.js
+++ src/pages/pagesIndex.js
@@ -1,3 +1,4 @@
 export {default as Home} from "./home/Home"
 export {default as GameMode} from "./gameMode/GameMode"
 export {default as Settings} from "./settings/Settings"
+export {default as CreateGame} from "./createGame/CreateGame"
