905dc603c2c55e6644c1c2cab7975c4303e0f647
alen <alen.l@captain.in>
2024-09-27 20:00:40 +0530

    added animations to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,27 +1,62 @@
-import './Home.css'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import img from "../../assets/images/stoned-skull.png";
+import "./Home.css";
 
 function Home() {
-    const navigate = useNavigate()
-    const handleStartCLick = () => {
-        navigate('/gamemode')
-    }
-    const handleSettingClick = () => {
-        navigate('/settings')
-    }
-    const handleHelpClick = () => {
-        navigate('/help')
-    }
+  const [loadingPercentage, setLoadingPercentage] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const handleStartCLick = () => {
+    navigate('/gamemode');
+  };
+
+  const handleSettingClick = () => {
+    navigate('/settings');
+  };
+
+  const handleHelpClick = () => {
+    navigate('/help');
+  };
+
+  // Simulate the loading progress
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingPercentage((prev) => {
+        if (prev < 100) {
+          return prev + 1; // Increment progress
+        } else {
+          clearInterval(interval); // Clear the interval when 100 is reached
+          setTimeout(() => setIsLoading(false), 500); // Wait 0.5s before removing loader
+          return prev;
+        }
+      });
+    }, 20); // Speed of progress (20ms for fast loading)
+
+    return () => clearInterval(interval); // Cleanup on component unmount
+  }, []);
+
   return (
-    <div className='home'>
-        <div className='home__display-area'>
+    <div className="home">
+      {isLoading ? (
+        <div className="loader">
+          <div className="loader__bar" style={{ width: `${loadingPercentage}%` }}></div>
+          <p>{loadingPercentage}%</p>
+        </div>
+      ) : (
+        <>
+          {/* Icon or Image with Animation */}
+          <img src={img} alt="Game Icon" className="home__icon" />
+          <div className="home__display-area">
             <p onClick={handleStartCLick}>Start Game</p>
             <p onClick={handleSettingClick}>Settings</p>
             <p onClick={handleHelpClick}>Help</p>
-        </div>
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
