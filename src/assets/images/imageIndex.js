d3c30996c5f1bd088c5cac9de18ba0149a041096
Alen J Ninan <alen.j@captainfresh.in>
2024-10-18 17:01:06 +0530

    added background image and made few css changes

---
+++ src/assets/images/imageIndex.js
@@ -0,0 +1,12 @@
+import background from './background.png';
+import question_mark from './question.png';
+import mag1 from './magnifier.png';
+import mag2 from './search.png';
+
+
+export { 
+    background, 
+    question_mark,
+    mag1,
+    mag2
+};
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,40 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import img from "../../assets/images/stoned-skull.png";
+import { question_mark, mag1, mag2 } from "../../assets/images/imageIndex";
 import "./Home.css";
 
 function Home() {
-
-
   const navigate = useNavigate();
 
   const handleStartCLick = () => {
-    navigate('/gamemode');
+    navigate("/gamemode");
   };
 
   const handleSettingClick = () => {
-    navigate('/settings');
+    navigate("/settings");
   };
 
   const handleHelpClick = () => {
-    navigate('/help');
+    navigate("/help");
   };
-
- 
 
   return (
     <div className="home">
-     
-        <>
-          {/* Icon or Image with Animation */}
-          <img src={img} alt="Game Icon" className="home__icon" />
-          <div className="home__display-area">
-            <p onClick={handleStartCLick}>Start Game</p>
-            <p onClick={handleSettingClick}>Settings</p>
-            <p onClick={handleHelpClick}>Help</p>
-          </div>
-        </>
-      
+      <div className="home__display-area">
+        <p onClick={handleStartCLick}>Start Game</p>
+        <p onClick={handleSettingClick}>Settings</p>
+        <p onClick={handleHelpClick}>Help</p>
+      </div>
     </div>
   );
 }
