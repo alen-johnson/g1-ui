ebc3fcb286148f7b9280be932ecf9a189810a8a0
Alen J Ninan <alen.j@captainfresh.in>
2024-11-02 16:25:15 +0530

    updated start modal

--- src/pages/gameMode/GameMode.js
+++ src/pages/gameMode/GameMode.js
@@ -1,32 +1,46 @@
-import { useNavigate } from "react-router-dom"
-import { StartModal } from "../../components/componentsIndex"
-import "./GameMode.css"
+import { useNavigate } from "react-router-dom";
+import { StartModal } from "../../components/componentsIndex";
+import "./GameMode.css";
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { Modal } from "antd";
 
 function GameMode() {
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const navigate = useNavigate()
-    const [isOpen, setIsOpen] = useState(false)
-    
-    const openModal = () => {
-        setIsOpen(true);
-    }
-    const closeModal = () => {
-        setIsOpen(false)
-    }
-    const handleMutliClick = () => {
-        navigate("/creategame")
-    }
+  const openModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleMutliClick = () => {
+    navigate("/creategame");
+  };
   return (
     <div className="game-mode">
-        <div className="game-mode__display-area">
-            <p onClick={openModal}>Single System Mode</p>
-            {isOpen && <StartModal isOpen={isOpen} closeModal={closeModal}/>}
-            <p onClick={handleMutliClick}>Multi System Mode</p>
-        </div>
+      <div className="game-mode__display-area">
+        <p onClick={openModal}>Single System Mode</p>
+        <Modal
+          open={isOpen}
+          onCancel={openModal}
+          mask={true} // Enables the mask background
+          maskClosable={false} // Prevents closing modal on mask click
+          maskStyle={{ backgroundColor: "rgba(0, 0, 0, .8)" }}
+          width={800}
+          bodyStyle={{ height: 520, overflowY: "auto" }}
+          footer= {null}
+        >
+          <StartModal />
+        </Modal>
+
+        <p
+        // onClick={handleMutliClick}
+        >
+          Multi System Mode
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default GameMode
+export default GameMode;
