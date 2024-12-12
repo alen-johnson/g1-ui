0926f8f4e0de8c366bc508db4e601487f9e7b6d7
Alen J Ninan <alen.j@captainfresh.in>
2024-12-12 11:58:18 +0530

    home page css changes

---
+++ src/assets/images/imageIndex.js
@@ -0,0 +1,8 @@
+export { default as background } from "./back.webp";
+export { default as infoIcon } from "./info.png";
+export { default as settingIcon } from "./engineer.png";
+export { default as startIcon } from "./giphy.webp";
+export { default as impo } from "./impo.png";
+export { default as searchIcon } from "./search.png";
+
+
